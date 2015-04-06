4e87ac8433e15a959b19bc1412a1006b2ff77d28
reburning <564555975@qq.com>
2015-04-06 15:34:51 +0800

    rails入门第一版完成

--- config/routes.rb
+++ config/routes.rb
@@ -57,5 +57,5 @@ Rails.application.routes.draw do
   #     # Directs /admin/products/* to Admin::ProductsController
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
-  #   end
+  #   end 
 end
